22e405b609b3c335467b62c6614ae730b8b5b20f
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-06 14:52:13 -0600

    append function

--- script.js
+++ script.js
@@ -6,6 +6,23 @@ class LinkedList {
     this.head = null;
   }
 
+  //// append function
+  append(value) {
+      const newNode = new Node(value);
+
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+
+      while(current.nextNode !== null) {
+        current = current.nextNode;
+      }
+      current.nextNode = newNode;
+    }    
+  }
+
+  
 
 }
 
