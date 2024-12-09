0743c386075d50ce89a8fb2b2d129f0b62da06f5
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-09 17:47:42 -0600

    appending more values

--- script.js
+++ script.js
@@ -219,6 +219,11 @@ class Node {
 const list = new LinkedList();
 
 list.append('dog');
+list.append('cat');
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
 list.prepend('alien');
 
