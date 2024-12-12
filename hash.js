72638aeb90c9f4b5c28a452d5d40aea43f495f45
Harneet Sahi <134643674+harneetsahi@users.noreply.github.com>
2024-12-12 16:49:28 -0600

    created hashMap class

--- hash.js
+++ hash.js
@@ -1,1 +1,10 @@
+class HashMap {
 
+  constructor(loadFactor = 0.75, capacity = 16) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+    this.bucket = new Array(this.capacity);
+  }
+
+  
+}
